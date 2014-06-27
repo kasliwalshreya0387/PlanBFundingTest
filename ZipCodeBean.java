70ca826c39500f8992769631d612781a20b80c8f
unknown <ctsuser@LTDLUK01474.cts.com>
2014-06-27 09:24:36 +0100

    Signed-off-by:Shreya Kasliwal

---
+++ ZipCodeBean.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.pfb;
+
+/**
+ *
+ * @author ctsuser
+ */
+public class ZipCodeBean {
+    
+    private String zipCode1;
+     private String zipCode2;
+      private String distance;
+
+    /**
+     * @return the zipCode1
+     */
+    public String getZipCode1() {
+        return zipCode1;
+    }
+
+    /**
+     * @param zipCode1 the zipCode1 to set
+     */
+    public void setZipCode1(String zipCode1) {
+        this.zipCode1 = zipCode1;
+    }
+
+    /**
+     * @return the zipCode2
+     */
+    public String getZipCode2() {
+        return zipCode2;
+    }
+
+    /**
+     * @param zipCode2 the zipCode2 to set
+     */
+    public void setZipCode2(String zipCode2) {
+        this.zipCode2 = zipCode2;
+    }
+
+    /**
+     * @return the distance
+     */
+    public String getDistance() {
+        return distance;
+    }
+
+    /**
+     * @param distance the distance to set
+     */
+    public void setDistance(String distance) {
+        this.distance = distance;
+    }
+    
+}
